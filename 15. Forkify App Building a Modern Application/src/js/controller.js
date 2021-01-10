9caed7b2a44037739ef4588952f41173c20eecd1
grigoar <grigore.nath.j@gmail.com>
2021-01-10 20:27:00 +0200

    added model, view, and controller, added MVC Architecture theory

--- 15. Forkify App Building a Modern Application/src/js/controller.js
+++ 15. Forkify App Building a Modern Application/src/js/controller.js
@@ -1,4 +1,12 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from "./model.js";
+import recipeView from "./views/recipeView.js";
+
+//polyfilling everyting else
+import "core-js/stable";
+//polyfilling async await
+import "regenerator-runtime/runtime";
+// console.log(icons);
+const recipeContainer = document.querySelector(".recipe");
 
 const timeout = function (s) {
   return new Promise(function (_, reject) {
@@ -11,3 +19,39 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+// console.log("Test");
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+    // console.log(id);
+
+    //clause guard if there is not a recipe id
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //1)Loading recipe
+    //a async function calling another async function
+    //This would receive a promise, so we need to wait
+    await model.loadRecipe(id); //this not storing anything, we need the state
+
+    // const  recipe  = model.state.recipe;
+    // const { recipe } = model.state;
+
+    //2) Rendering recipe
+    recipeView.render(model.state.recipe);
+  } catch (error) {
+    alert("Error from async controller: ", error);
+  }
+};
+
+// controlRecipes();
+["hashchange", "load"].forEach((ev) => window.addEventListener(ev, controlRecipes));
+// window.addEventListener("hashchange", controlRecipes);
+//listening when the page is loading
+// window.addEventListener("load", controlRecipes);
+
+//------------------------receiving the recipe from API
+//------------------------rendering the recipe
+//------------------------- Listening for load and hashchange events
+//-----------------------The MVC Architecture
---
+++ 15. Forkify App Building a Modern Application/src/js/model.js
@@ -0,0 +1,36 @@
+export const state = {
+  recipe: {},
+};
+
+//this only change(manipulate) the state
+export const loadRecipe = async function (id) {
+  try {
+    const res = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+      // "https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc8f7"
+    );
+
+    //stop the execution until the promise is returned
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    console.log(res, data);
+
+    // let recipe = data.data.recipe;
+    const { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceURL: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+    console.log("My object received", state.recipe);
+  } catch (error) {
+    alert("Error from async model: ", error.message);
+  }
+};
